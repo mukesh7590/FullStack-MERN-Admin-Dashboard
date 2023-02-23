ec0149f82f3f322ffa06c29f588bf78c1b18493a
Mukesh <mukeshbisht7590@gmail.com>
2023-02-23 11:09:09 +0530

    project is done now. all tasks working correctly

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,9 @@
+const { styled, Box } = require("@mui/material");
+
+const FlexBetween = styled(Box)({
+   display: "flex",
+   justifyContent: "space-between",
+   alignItems: "center",
+});
+
+export default FlexBetween
